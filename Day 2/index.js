58a2cd6abc4bd35974421dbbcc425af5007d8028
premkarjavkar <premkarjavkar02@gmail.com>
2026-06-11 15:09:21 +0530

    push day 1 and day 2

---
+++ Day 2/index.js
@@ -0,0 +1,117 @@
+// Data Types 
+
+
+// "" '' ``
+
+let email = `abc@gmail.com`;
+let name = "abc";
+let password = "pass@123";
+
+console.log(email, "email");
+console.log(name, "name");
+console.log(password, "password");
+
+console.log( typeof(email) );
+console.log( typeof(name) );
+console.log( typeof(password) );
+
+
+// Number
+
+const age = 25;
+console.log(age, "age");
+console.log( typeof(age) );
+
+
+
+
+
+const phoneNumber = "23456543";
+console.log(phoneNumber, "phoneNumber");
+console.log( typeof(phoneNumber) );
+
+
+
+
+
+
+
+const myPassword = "Pass@123"
+console.log(myPassword, "myPassword");
+console.log( typeof(myPassword) );
+
+
+
+
+// Boolean
+
+
+const isLoggedIn = true;
+console.log(isLoggedIn, "isLoggedIn");
+console.log(typeof isLoggedIn);
+
+
+const isLoggedIn = false;
+console.log(isLoggedIn, "isLoggedIn");
+console.log(typeof isLoggedIn);
+
+
+
+
+
+
+// Undefined
+
+var firstName;
+console.log(firstName, "firstName");
+console.log(typeof firstName);
+
+
+
+
+// Object
+
+var userData = null;
+console.log(userData, "userData");
+console.log(typeof userData);
+
+
+
+let a = 10;
+console.log(a, "a");
+let b = a;
+console.log(b, "b");
+b = 20;
+console.log(a, "a");
+console.log(b, "b");
+
+
+
+
+// Arithmetic Operators
+
+
+// Addition
+console.log(10 + 5); 
+
+// Subtraction
+console.log(5 - 10);
+
+// Multiplication
+console.log(10 * 5);
+
+ // Division
+console.log(30 / 2);
+
+
+
+// even 0
+// odd 1
+
+console.log(10 % 2); // 0 - even
+console.log(11 % 2); // 1 - odd
+console.log(12 % 2); // 0 - even
+console.log(13 % 2); // 1 - odd
+console.log(14 % 2); // 0 - even
+console.log(15 % 2); // 1 - odd
+

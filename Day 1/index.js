fd4b5b0b4492c25ce08e2517d6a4fb9026d29980
premkarjavkar <premkarjavkar02@gmail.com>
2026-06-11 14:40:45 +0530

    update

---
+++ Day 1/index.js
@@ -0,0 +1,80 @@
+ var age = 25
+ console.log(age, age, age)
+
+var number = 1 //declare and assign
+console.log(number);
+
+number = 2;  //re assign
+console.log(number);
+
+var number = 3 //re declare and assign
+console.log(number);
+
+let age = 25;
+console.log(age);
+
+age = 26;
+console.log(age);
+
+let age = 27;
+console.log(age);
+
+const number = 20;
+console.log(number);
+
+number = 21;
+console.log(number);
+
+const number = 23;
+console.log(number)
+
+//               var         let           cont
+// reassign      yes         yes            no
+
+// redeclare     yes          no            no
+
+// hoisting      yes          no            no
+
+// scope        function    block         block
+
+age = 25;
+console.log(number);
+
+const age = 25
+
+
+age = 25
+console.log(numder)
+var age;
+
+
+
+
+
+
+var number1 = 1;
+{
+    Number2 = 2;
+    let number3 = 3;
+    const number4 = 4;
+    {
+        console.log(number3);
+        console.log(number2);
+    }
+    console.log(number4);
+}
+var number2;
+console.log(number1)
+
+
+
+console.log(number);
+number = 25;
+console.log(number1);
+
+var number;
+
+
+
+
+
